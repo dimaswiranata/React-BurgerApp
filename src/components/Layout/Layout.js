0cbf08866e194842c56df4306061b87bc755fb03
Dimas Agusta Wiranata <dimaswiranata8@gmail.com>
2020-04-19 22:40:50 +0700

    creating a resposive sidedrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,10 +3,12 @@ import React from 'react';
 import Aux from '../../hoc/Auxiliary';
 import './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const Layout = (props) => (
   <Aux>
     <Toolbar/>
+    <SideDrawer/>
     <main className='Content'>
       {props.children}
     </main>
