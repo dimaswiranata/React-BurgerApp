cf88ce0e4d285c4ca2c1cd39962080a3d21a3e31
Dimas Agusta Wiranata <dimaswiranata8@gmail.com>
2020-04-20 17:58:18 +0700

    using component lifecycle methods

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './Modal.css';
 import Aux from '../../../hoc/Auxiliary';
 import Backdrop from '../Backdrop/Backdrop';
 
-const Modal = ( props ) => (
-    <Aux>
-        <Backdrop show={props.show} clicked={props.modalClosed} />
+class Modal extends Component {
+  // this could be a functional component, doesn't have to be a class
+  shouldComponentUpdate (nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
+
+  componentWillUpdate() {
+    console.log('[Modal] WillUpdate');
+  }
+
+  render() {
+    return (
+      <Aux>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
         <div
-            className='Modal'
-            style={{
-                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                opacity: props.show ? '1' : '0'
-            }}>
-            {props.children}
+          className='Modal'
+          style={{
+            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: this.props.show ? '1' : '0'
+          }}>
+          {this.props.children}
         </div>
-    </Aux>
-);
+      </Aux>
+    );
+  }
+}
 
 export default Modal;
